f38ab51220469a2988f22bc09597ecef3110cf8b
nawapolsungjun <nawapolsungjun@gmail.com>
2024-12-18 11:47:39 +0700

    Update App.java

--- Test5/app/src/main/java/test5/App.java
+++ Test5/app/src/main/java/test5/App.java
@@ -11,7 +11,6 @@ public class App {
     int[][] matrix = new int[3][3];
     int[][] transpose = new int[3][3];
 
-    System.out.println("Enter values for a 3x3 matrix:");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             System.out.print("Enter Matrix index of [" + i + "][" + j + "] : ");
